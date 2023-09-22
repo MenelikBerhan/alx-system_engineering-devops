aa1c5c1b8d3b9be35e2830a5c6405429afcc1edc
Menelik_Berhan <MenelikBerhan@gmail.com>
2023-09-22 18:56:03 +0300

    Task 0

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,8 @@
+file { 'school' :
+    ensure  => file,
+    path    => '/etc/school',
+    mode    => '0744',
+    owner   => 'www-data',
+    group   => 'www-data',
+    content => 'I love Puppet'
+}
