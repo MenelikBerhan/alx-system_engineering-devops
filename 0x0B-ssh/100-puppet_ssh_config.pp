1f51b4f27b528476118514a233207222b20ed43e
Menelik_Berhan <MenelikBerhan@gmail.com>
2023-09-23 19:55:42 +0300

    Correct config file name

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,7 +1,7 @@
 # creates an SSH configuration file so that you can connect to a server without typing a password.
 file { 'config' :
     ensure  => file,
-    path    => '/etc/ssh/ssh_config1',
+    path    => '/etc/ssh/ssh_config',
     content => 'HOST *
     HostName 18.206.208.78
     IdentityFile ~/.ssh/school
