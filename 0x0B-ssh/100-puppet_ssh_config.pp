e244312c5adcba234f78e4de92206b95a940e860
Menelik_Berhan <MenelikBerhan@gmail.com>
2023-09-23 19:49:17 +0300

    Task 4

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,11 @@
+# creates an SSH configuration file so that you can connect to a server without typing a password.
+file { 'config' :
+    ensure  => file,
+    path    => '/etc/ssh/ssh_config1',
+    content => 'HOST *
+    HostName 18.206.208.78
+    IdentityFile ~/.ssh/school
+    User ubuntu
+    PasswordAuthentication no
+'
+}
