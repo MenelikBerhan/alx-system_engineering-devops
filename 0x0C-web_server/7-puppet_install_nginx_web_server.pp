044cee5d95f82614df2767b51f58c80ce9a04e1e
Menelik_Berhan <MenelikBerhan@gmail.com>
2023-09-27 05:03:30 +0300

    Break line for puppet-lint requirement

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -5,6 +5,7 @@ exec {'install nginx':
   command  => 'sudo apt-get update ;
   sudo apt-get -y install nginx ;
   sudo echo "Hello World!" > /var/www/html/index.nginx-debian.html ;
-  sudo sed -i "s/server_name _;/server_name _;\n\trewrite ^\/redirect_me https:\/\/www.youtube.com/watch?v=QH2-TGUlwu4 permanent;/" /etc/nginx/sites-available/default ;
+  sudo sed -i "s/server_name _;/server_name _;\n\trewrite ^\/redirect_me https:\/\/www.youtube.com/watch?v=QH2-TGUlwu4 permanent;/"
+  /etc/nginx/sites-available/default ;
   sudo service nginx start ;'
 }
