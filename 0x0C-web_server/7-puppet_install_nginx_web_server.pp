e0472a6260bbf1ef0479733eecdb2ab03d90f61b
Menelik_Berhan <MenelikBerhan@gmail.com>
2023-09-27 05:09:32 +0300

    update redirect

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,5 +1,5 @@
 # install and setup an nginx server
 exec {'install nginx':
   provider => 'shell',
-  command  => 'sudo apt-get update ; sudo apt-get -y install nginx ; sudo echo "Hello World!" > /var/www/html/index.nginx-debian.html ; sudo sed -i "s/server_name _;/server_name _;\n\trewrite ^\/redirect_me https:\/\/www.oluwatobisalau.netlify.app permanent;/" /etc/nginx/sites-available/default ; sudo service nginx start'
+  command  => 'sudo apt-get update ; sudo apt-get -y install nginx ; sudo echo "Hello World!" > /var/www/html/index.nginx-debian.html ; sudo sed -i "s/server_name _;/server_name _;\n\trewrite ^\/redirect_me https:\/\/www.youtube.com/watch?v=QH2-TGUlwu4 permanent;/" /etc/nginx/sites-available/default ; sudo service nginx start'
 }
