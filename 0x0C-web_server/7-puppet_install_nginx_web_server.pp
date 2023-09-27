329aba3529b7339e32c7561e12f02960712d3ac2
Menelik_Berhan <MenelikBerhan@gmail.com>
2023-09-27 04:43:23 +0300

    Task 5

---
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -0,0 +1,10 @@
+# install and setup an nginx server
+
+exec {'install nginx':
+  provider => 'shell',
+  command  => 'sudo apt-get update ;
+  sudo apt-get -y install nginx ;
+  sudo echo "Hello World!" > /var/www/html/index.nginx-debian.html;
+  sudo sed -i "s/server_name _;/server_name _;\n\trewrite ^\/redirect_me https:\/\/www.youtube.com/watch?v=QH2-TGUlwu4 permanent;/" /etc/nginx/sites-available/default;
+  sudo service nginx start'
+}
