5c2b20ce9ca4b4b38651b338ff4eb550230574ac
Menelik_Berhan <MenelikBerhan@gmail.com>
2023-10-03 05:24:39 +0300

    Fix errors

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,17 +1,18 @@
 # install and setup an nginx server
-exec {'install nginx':
+exec {'install':
   provider => 'shell',
-  command  => 'sudo apt-get update ; sudo apt-get -y install nginx ; sudo service nginx start'
+  command  => 'sudo apt-get update ; sudo apt-get -y install nginx ; sudo echo "Hello World!" > /var/www/html/index.html ; sudo sed -i "s/server_name _;/server_name _;\n\trewrite ^\/redirect_me https:\/\/www.youtube.com permanent;/" /etc/nginx/sites-available/default ; sudo service nginx start'
 }
 
 file_line { 'header':
-  ensure => present,
-  path   => '/etc/nginx/sites-available/default',
-  after  => ':80 default_server;',
-  line   => "add_header X-Served-By ${hostname};"
+  ensure  => present,
+  path    => '/etc/nginx/sites-available/default',
+  after   => ':80 default_server;',
+  line    => "add_header X-Served-By ${hostname};",
+  require => Exec['install']
 }
 
-exec {'restart nginx':
-  provider => 'shell',
-  command  => 'sudo service nginx restart'
+service { 'nginx':
+  ensure  => running,
+  require => File_line['header']
 }
