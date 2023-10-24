3a633c126941080455587c213c705ca868d3e55b
Menelik_Berhan <MenelikBerhan@gmail.com>
2023-10-24 03:44:11 +0300

    Close Response before making next Request

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -7,25 +7,27 @@ if __name__ == '__main__':
     import json
     import requests
 
-    url = 'https://jsonplaceholder.typicode.com/users'
-    users = requests.get(url=url).json()
+    url = "https://jsonplaceholder.typicode.com/users"
+    res = requests.get(url)
+    users = res.json()
+    res.close()
     users_list = []
     for user in users:
         users_list.append({'id': user.get('id'),
                            'username': user.get('username')})
-    # print(users_list)
-    task_url = 'https://jsonplaceholder.typicode.com/todos'
+    task_url = "https://jsonplaceholder.typicode.com/todos"
     json_dict = {}
     for usr in users_list:
         task_list = []
-        # print(task_url)
-        params = {'userId': usr.get('id')}
-        tasks = requests.get(url=task_url).json()
+        params = {'userId': str(usr.get('id'))}
+        res = requests.get(task_url, params=params)
+        tasks = res.json()
+        res.close()
         for task in tasks:
             task_list.append({'username': usr['username'],
                               'task': task.get('title'),
                               'completed': task.get('completed')})
-        json_dict.update({usr['id']: task_list})
-    # print(json_dict[1])
+        json_dict.update({usr.get('id'): task_list})
+
     with open('todo_all_employees.json', 'w') as file:
         json.dump(json_dict, file)
