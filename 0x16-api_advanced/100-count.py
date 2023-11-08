3fdcd9bf263f960934e6df0b5edbc61f89b629c7
Menelik_Berhan <MenelikBerhan@gmail.com>
2023-11-08 18:38:27 +0300

    Exit after printing words

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -35,6 +35,7 @@ def count_words(subreddit, word_list, hot_list={}, subreddit_checked=False,
             sorted_keys.sort(key=lambda x: hot_list[x], reverse=True)
             for key in sorted_keys:
                 print('{}: {}'.format(key, hot_list[key]))
+                exit(0)
 
         words = [word.lower() for word in word_list]
         for post in resp.get('children'):
