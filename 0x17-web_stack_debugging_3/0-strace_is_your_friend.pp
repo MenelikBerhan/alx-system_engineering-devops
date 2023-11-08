a5c96fe4b82ff4f6c716f654f44653865e87c06e
Menelik_Berhan <MenelikBerhan@gmail.com>
2023-11-08 22:07:00 +0300

    Use variables to reduce line length

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,10 +1,12 @@
 # fix a typo error in /var/www/html/wp-settings.php
+$old_text = 'require_once( ABSPATH . WPINC . \x27\/class-wp-locale.phpp\x27 );'
+$new_text = 'require_once( ABSPATH . WPINC . \x27\/class-wp-locale.php\x27 );'
 exec { 'install sed':
   command  => 'apt install sed -y',
   provider => shell,
   path     => ['/usr/bin', '/usr/sbin',],
 }->exec { 'change line in wp-settings.php':
-  command  => "sed -i 's/require_once( ABSPATH . WPINC . \x27\/class-wp-locale.phpp\x27 );/require_once( ABSPATH . WPINC . \x27\/class-wp-locale.php\x27 );/' /var/www/html/wp-settings.php",
+  command  => "sed -i 's/${old_text}/${new_text}/' /var/www/html/wp-settings.php",
   provider => shell,
   path     => ['/bin', '/usr/bin', '/usr/sbin',],
 }-> exec { 'restart apache':
