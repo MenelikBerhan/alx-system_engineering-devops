5d64c2d495177838d5b52fddbe67b9841a50abe8
Menelik_Berhan <MenelikBerhan@gmail.com>
2023-11-08 22:50:35 +0300

    Revert to yesterdays last version & import in fun

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -1,47 +1,49 @@
 #!/usr/bin/python3
-"""Module for task 3"""
+"""Module containing a function that parses the title of all hot articles,
+and prints a sorted count of given keywords"""
 
 
-def count_words(subreddit, word_list, word_count={}, after=None):
-    """Queries the Reddit API and returns the count of words in
-    word_list in the titles of all the hot posts
-    of the subreddit"""
+def count_words(subreddit, word_list, hot_list={}, subreddit_checked=False,
+                after=None):
+    """parses the title of all hot articles, and prints a sorted count of
+    given keywords (case-insensitive, delimited by spaces). If no posts match
+    or the subreddit is invalid, prints nothing."""
     import requests
 
-    sub_info = requests.get("https://www.reddit.com/r/{}/hot.json"
-                            .format(subreddit),
-                            params={"after": after},
-                            headers={"User-Agent": "My-User-Agent"},
-                            allow_redirects=False)
-    if sub_info.status_code != 200:
-        return None
-
-    info = sub_info.json()
-
-    hot_l = [child.get("data").get("title")
-             for child in info
-             .get("data")
-             .get("children")]
-    if not hot_l:
-        return None
-
-    word_list = list(dict.fromkeys(word_list))
-
-    if word_count == {}:
-        word_count = {word: 0 for word in word_list}
-
-    for title in hot_l:
-        split_words = title.split(' ')
-        for word in word_list:
-            for s_word in split_words:
-                if s_word.lower() == word.lower():
-                    word_count[word] += 1
-
-    if not info.get("data").get("after"):
-        sorted_counts = sorted(word_count.items(), key=lambda kv: kv[0])
-        sorted_counts = sorted(word_count.items(),
-                               key=lambda kv: kv[1], reverse=True)
-        [print('{}: {}'.format(k, v)) for k, v in sorted_counts if v != 0]
+    headers = {"User-Agent": "Menelik Berhan"}
+    if not subreddit_checked:
+        # search given subreddit
+        params = {"raw_json": 1, 'query': subreddit, 'exact': True,
+                  'include_unadvertisable': True}
+        url = "https://www.reddit.com/api/search_subreddits.json"
+        response = requests.post(url=url, headers=headers, params=params)
+        subreddit_exists = response.status_code == 200
     else:
-        return count_words(subreddit, word_list, word_count,
-                           info.get("data").get("after"))
+        subreddit_exists = True
+
+    # if found get and print top ten posts, else print None
+    if subreddit_exists:
+        if after is None:
+            params = {'limit': 100}
+        else:
+            params = {'limit': 100, 'after': after}
+        url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
+        response = requests.get(url, headers=headers, params=params)
+        resp = response.json().get('data')
+        if not resp.get('children'):
+            sorted_keys = list(hot_list.keys()).sort()
+            sorted_keys.sort(key=lambda x: hot_list[x], reverse=True)
+            for key in sorted_keys:
+                print('{}: {}'.format(key, hot_list[key]))
+
+        words = [word.lower() for word in word_list]
+        for post in resp.get('children'):
+            title = str(post.get('data').get('title'))
+            title_list = title.lower().split()
+            for word in words:
+                word_count = title_list.count(word)
+                if word_count:
+                    hot_list[word] = hot_list.get(word, 0) + word_count
+
+        count_words(subreddit, word_list, hot_list, True,
+                    after=resp.get('after'))
